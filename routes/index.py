35b494d225476c499565c9a2045d1c680730c989
jesusjoelgonca <107076643+jesusjoelgonca@users.noreply.github.com>
2022-11-09 17:16:32 -0500

    actualizo index se quito el jwt required

--- routes/index.py
+++ routes/index.py
@@ -4,6 +4,5 @@ from flask_jwt_extended import jwt_required, create_access_token
 principal = Blueprint('principal',__name__)
 
 @principal.route('/principal')
-@jwt_required()
 def index():
     return jsonify({'Mensaje':'Bienvenido!'})
