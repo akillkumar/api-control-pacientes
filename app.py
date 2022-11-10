3b0b4a77dc5c620cd297f0367ceb4560c70fd8a1
Akhil Kumar <46084762+akillkumar@users.noreply.github.com>
2022-11-09 18:09:23 -0800

    Update IP and Port bindings
    Hello!
    
    Akhil here from Zeet. I saw that you were trying to deploy your app to GCP using Zeet. I think the reason your app isn't deploying is because you are listening on localhost. I proposed changes to listen on all ports, instead of just your local machine - this will allow your app to service any incoming requests. For more information about this: https://stackoverflow.com/questions/31904761/what-does-app-runhost-0-0-0-0-mean-in-flask
    
    Hope this helps!

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def pagina_no_encontrada(error):
 
 if __name__=='__main__':
     app.register_error_handler(404,pagina_no_encontrada)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
